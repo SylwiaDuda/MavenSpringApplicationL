25e81850b69133457eea519655d2e2e214480096
Sylwia Duda <sylwia.agnieszka.duda@gmail.com>
2019-05-08 18:59:15 +0200

    twig view test

--- src/main/java/pl/mavenSpringApplicationL/Main.java
+++ src/main/java/pl/mavenSpringApplicationL/Main.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import pl.mavenSpringApplicationL.config.SecurityConfig;
+import pl.mavenSpringApplicationL.config.SecurityConfiguration;
+import pl.mavenSpringApplicationL.config.WebAppConfiguration;
+import pl.mavenSpringApplicationL.controller.MainController;
 import pl.mavenSpringApplicationL.controller.PageController;
 import pl.mavenSpringApplicationL.entity.Task;
 import pl.mavenSpringApplicationL.repositories.TaskRepository;
 
 
 @EnableAutoConfiguration
-@ComponentScan(basePackageClasses = {Task.class, PageController.class, SecurityConfig.class})
+@ComponentScan(basePackageClasses = {Task.class, PageController.class, SecurityConfiguration.class,
+        MainController.class, WebAppConfiguration.class})
 @EnableJpaRepositories(basePackageClasses = TaskRepository.class)
 
 public class Main {
