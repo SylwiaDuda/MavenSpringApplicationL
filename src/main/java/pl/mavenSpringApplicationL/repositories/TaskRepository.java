e6a1d22417bcb87bfec3da681f1fa6407e54b736
Sylwia Duda <sylwia.agnieszka.duda@gmail.com>
2019-05-08 16:14:28 +0200

    save to database

--- src/main/java/pl/mavenSpringApplicationL/repositories/TaskRepository.java
+++ src/main/java/pl/mavenSpringApplicationL/repositories/TaskRepository.java
@@ -3,5 +3,4 @@ package pl.mavenSpringApplicationL.repositories;
 import org.springframework.data.repository.CrudRepository;
 import pl.mavenSpringApplicationL.entity.Task;
 
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
+public interface TaskRepository extends CrudRepository <Task,Long> {}
